e2dafb681540541e50bc7d6ad769a13dfab5d070
hexadivine <hexadivine@gmail.com>
2024-06-10 05:24:09 +1000

    created schema for db model

--- backend/models/appModel.js
+++ backend/models/appModel.js
@@ -2,19 +2,22 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const appSchema = new Schema({
-    service: {
-        type: String,
-        required: true,
+const appSchema = new Schema(
+    {
+        service: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-});
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("appModel", appSchema);
