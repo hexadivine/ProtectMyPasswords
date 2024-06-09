2092ab87a6d6c87b48ed4a31f5454ecf1a9c29d8
hexadivine <hexadivine@gmail.com>
2024-06-10 05:24:27 +1000

    using controllers for routes

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -1,24 +1,10 @@
 const express = require("express");
 const route = express.Router();
 
-const dbHandler = require("../models/appModel");
+const { postData, getDatas, getData } = require("./../controllers/appControllers");
 
-route.get("/", (req, res) => {
-    return res.json({ msg: "yo" });
-});
-
-route.post("/", async (req, res) => {
-    try {
-        const { service, email, password } = req.body;
-        const result = await dbHandler.create({
-            service: service,
-            email: email,
-            password: password,
-        });
-        return res.status(200).json({ status: "ok", msg: result });
-    } catch (error) {
-        return res.status(400).json({ status: "error", msg: error.message });
-    }
-});
+route.get("/", getDatas);
+route.get("/:id", getData);
+route.post("/", postData);
 
 module.exports = route;
