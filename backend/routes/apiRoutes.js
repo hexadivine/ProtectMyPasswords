dd40df667b4eb3f050fb070e41d1c43f84234411
hexadivine <hexadivine@gmail.com>
2024-06-10 01:51:28 +1000

    set up post api to strore data

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -1,18 +1,24 @@
 const express = require("express");
-
 const route = express.Router();
+
+const dbHandler = require("../models/appModel");
 
 route.get("/", (req, res) => {
     return res.json({ msg: "yo" });
 });
 
-route.post("/", (req, res) => {
-    const { service, email, password } = req.body;
-    return res.status(200).json({
-        service: service,
-        email: email,
-        password: password,
-    });
+route.post("/", async (req, res) => {
+    try {
+        const { service, email, password } = req.body;
+        const result = await dbHandler.create({
+            service: service,
+            email: email,
+            password: password,
+        });
+        return res.status(200).json({ status: "ok", msg: result });
+    } catch (error) {
+        return res.status(400).json({ status: "error", msg: error.message });
+    }
 });
 
 module.exports = route;
