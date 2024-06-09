6dd84f365acd8b9854a8b3c77a4ca6b7713427c0
hexadivine <hexadivine@gmail.com>
2024-06-10 05:35:59 +1000

    added updateData controller to routes

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const route = express.Router();
 
-const { postData, getDatas, getData } = require("./../controllers/appControllers");
+const { postData, getDatas, getData, updateData } = require("./../controllers/appControllers");
 
 route.get("/", getDatas);
 route.get("/:id", getData);
 route.post("/", postData);
+route.put("/:id", updateData);
 
 module.exports = route;
