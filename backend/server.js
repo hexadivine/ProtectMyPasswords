7460f82b7412deed09ffc0439007b9d3d49bb00e
hexadivine <hexadivine@gmail.com>
2024-06-14 14:20:57 +1000

    changed to proper server status log msg

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
-            console.log("hi");
+            console.log("server started on " + process.env.PORT);
         });
     })
     .catch((error) => {
