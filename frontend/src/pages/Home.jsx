700f018f7ae7a5fb9e2570e72cdda9fb4aa1d3cb
hexadivine <hexadivine@gmail.com>
2024-06-14 14:22:42 +1000

    fetch api on visiting website

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,29 @@
-import styles from "./Home.module.css";
+import { useEffect, useState } from "react";
+// import styles from "./Home.module.css";
+import EntryDetails from "../contents/EntryDetails";
 
 const Home = () => {
+    // fetch data when page reloads
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("/api");
+            const result = await response.json();
+
+            if (response.ok && result.status === "ok") setData(result.msg);
+        };
+
+        fetchData();
+    }, []);
+    // console.log(data);
     return (
         <>
-            <div className={styles.container}>
-                <h1>Home</h1>
+            <div>
+                {/* {data && data.map((element) => <p key={element._id}>{element._id} </p>)} */}
+                {data.map((element) => (
+                    <EntryDetails key={element._id} data={element} />
+                ))}
+                {/* </div> */}
             </div>
         </>
     );
